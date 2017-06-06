a76be4b8c1fd42bde5315fd0491749c1bf8c6b86
Misha Wolfson <misha@ginkgobioworks.com>
2017-06-06 00:05:09 +0000

    Bump version: 0.4.5 → 0.4.6

--- pychemy/__init__.py
+++ pychemy/__init__.py
@@ -1,3 +1,3 @@
 import matplotlib as mpl
-__version__ = '0.4.5'
+__version__ = '0.4.6'
 mpl.use('AGG')
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
   name='pychemy',
-  version='0.4.5',
+  version='0.4.6',
 
   author='Benjie Chen, Ginkgo Bioworks, Christoph Gohlke',
   author_email='benjie@ginkgobioworks.com, devs@ginkgobioworks.com, cgohlke@uci.edu',
